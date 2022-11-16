4f9da4fe493c5b2012475fdc2640fc2f6928a356
Piotr Szulc <szulo123@gmail.com>
2022-11-16 21:13:41 +0100

    Initial commit

---
+++ keymaps/default/keymap.c
@@ -0,0 +1,21 @@
+#include QMK_KEYBOARD_H
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /*
+     * ┌───┬───┬───┐
+     * │ A │ B │ C │
+     * ├───┼───┼───┤
+     * │ D │ E │ F │
+     * └───┴───┴───┘
+     */
+    [0] = LAYOUT_ortho_2x3(
+        KC_CALC, KC_NO,   KC_NO,
+        KC_MPRV, KC_MPLY, KC_MNXT
+    )
+};
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][2] = {
+    [0] =   { ENCODER_CCW_CW(KC_VOLD, KC_VOLU) },
+};
+#endif
